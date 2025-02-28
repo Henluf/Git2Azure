ad1d2e55bf04264af8b7b5a363945bc3309b539a
Hendrik Herlufsen <hendrik.herlufsen@ppi.de>
2025-02-28 14:01:40 +0100

    Add azureContainerTest
    ContainerTest-Project to test github-action for docker build&push.

---
+++ azureContainerTest/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/exp01/test", func(c *gin.Context) {
+
+		if c.Query("EXP01-KEY") == "H3r34r3Dr4g0ns" {
+
+			c.JSON(200, gin.H{
+				"message": "You are the dragon master !!",
+			})
+
+		} else {
+
+			c.JSON(403, gin.H{
+				"error": "This is not the endpoint you're looking for !!! (No Dragons)",
+			})
+
+		}
+	})
+	_ = r.Run(":31415")
+}
